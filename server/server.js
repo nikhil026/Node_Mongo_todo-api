ebb442067fc94fd6cdbd5058ea724fb3d6267b91
nikhil051097 <nikhil051097@gmail.com>
2017-02-22 11:45:31 +0530

    Add POST/todos

---
+++ server/server.js
@@ -0,0 +1,44 @@
+var express=require('express');
+var bodyParser=require('body-parser');
+
+var {mongoose}=require('./db/mongoose');
+var {Todo}=require('./models/todo');
+var {User}=require('./models/user');
+
+var app=express();
+
+
+app.use(bodyParser.json());
+
+app.post('/todos',(req,res)=>{
+  var todo=new Todo({
+    text: res.body.text
+  });
+
+  todo.save().then((doc)=>{
+    res.send(doc);
+  },(e)=>{
+  res.status(400).send(e);
+});
+});
+
+app.get('/todos',(req,res)=>
+{
+  Todo.find().then((todos)=>
+{
+  res.send({
+    todos
+  })
+
+},(e)=>
+{
+res.status(400).send(e);
+});
+
+});
+
+
+app.listen(9999,()=>{
+  console.log('Server Started at 9999');
+  }
+);
